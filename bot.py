1c262ab9ec4f8066259739e3a609c223e1742d4c
h2o2-debug <nespider73@gmail.com>
2025-12-27 21:08:32 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -244,7 +244,7 @@ def main():
         MessageHandler(filters.ALL & ~filters.COMMAND, route_incoming)
     )
 
-    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True,)
+    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True,close_loop=False,)
 
 if __name__ == "__main__":
     main()
